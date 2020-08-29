62d707dd3142122179855ee824eb272be15ab3df
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-29 16:47:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,11 @@
 
 def key_for_min_value(name_hash)
   lowest_value = nil
-  
+  lowest_key = nil
   name_hash.collect do |key, value|
     if lowest_value == nil || value < lowest_value
       lowest_value = value
+      lowest_key = key
     end
   end
   lowest_value
